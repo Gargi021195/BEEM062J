f4107c5f840ab94dcd137fb5696ce0c45b0dc233
Gargi Bhardwaj <83785399+Gargi021195@users.noreply.github.com>
2021-11-08 03:48:42 +0000

    Add files via upload

---
+++ myfirstwebsite.js
@@ -0,0 +1,13 @@
+function changeImage(){
+var image = document.getElementById('myImage');
+
+if
+
+(image.src.match("Bitcoin_2"))
+
+{image.src="Bitcoin_1.jpg";}
+
+else
+{image.src="Bitcoin_2.jpg";}
+
+}
